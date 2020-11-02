78d474aa0feb3f7178492c3c45b28d18858695a9
thanhntphys <thanhntphys@gmail.com>
2020-11-02 16:39:21 +0700

    edit project

--- DjangoRestApisPostgreSQL/settings.py
+++ DjangoRestApisPostgreSQL/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS_ORIGIN_ALLOW_ALL = False
