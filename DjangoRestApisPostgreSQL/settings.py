11ff0e3ef1552f0029e1384ddfe38694f5788476
thanhntphys <thanhntphys@gmail.com>
2020-11-02 16:42:53 +0700

    edit project

--- DjangoRestApisPostgreSQL/settings.py
+++ DjangoRestApisPostgreSQL/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')6u3!rm2fq#%3f6)r%53=v0*9wq7)y$bb3y)%#tlbxhaaw$7h#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
